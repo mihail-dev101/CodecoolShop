619a15bb9418360f83f93aa70c5268b06b37db36
emiliaolteanu13 <emiliaolteanu13@yahoo.com>
2021-11-09 14:53:42 +0200

    javascript ajax implemented and post function in controller but doesnt render ok

---
+++ src/Codecool.CodecoolShop/Models/CartDeserializeList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class CartDeserializeList
+    {
+        public List<CartDeserializeModel> Cart { get; set; }
+    }
+}
---
+++ src/Codecool.CodecoolShop/Models/CartDeserializeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class CartDeserializeModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string price { get; set; }
+        public string supplier { get; set; }
+        public string category { get; set;}
+        public string quanity { get; set; }
+    }
+}
