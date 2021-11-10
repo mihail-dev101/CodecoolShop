a0d15a5d4f893c3bf33fe4d28088d86f81c99ed9
emiliaolteanu13 <emiliaolteanu13@yahoo.com>
2021-11-10 15:18:37 +0200

    getAll from cartDaoDb trial

--- src/Codecool.CodecoolShop/Daos/Implementations/DataBase/ProductCartDaoDb.cs
+++ src/Codecool.CodecoolShop/Daos/Implementations/DataBase/ProductCartDaoDb.cs
@@ -13,8 +13,8 @@ namespace Codecool.CodecoolShop.Daos.Implementations
         DbProviderFactory factory;
         string provider;
         string connectionString;
-
-        public ProductCartDaoDb()
+        private ProductCartDaoDb instance = null;
+        private ProductCartDaoDb()
         {
             DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", Microsoft.Data.SqlClient.SqlClientFactory.Instance);
             provider = ConfigurationManager.AppSettings["provider"];
@@ -22,7 +22,14 @@ namespace Codecool.CodecoolShop.Daos.Implementations
             factory = DbProviderFactories.GetFactory(provider);
         }
 
-        
+        public ProductCartDaoDb GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ProductCartDaoDb();
+            }
+            return instance;
+        }
 
         public void Add(CartItemModel item)
         {
@@ -57,7 +64,32 @@ namespace Codecool.CodecoolShop.Daos.Implementations
 
         IEnumerable<CartItemModel> IDao<CartItemModel>.GetAll()
         {
-            throw new NotImplementedException();
-        }
+            var cart = new List<CartItemModel>();
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                var command = factory.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = "Select *, cart.ID as 'cart.ID',cart.user_ID as 'cart.user_ID',cart.product_id as 'cart.product_id' From product, cart";
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        CartItemModel item = new CartItemModel();
+                        string allProducts = (string)reader["product_id"];
+                        string[] productIds = allProducts.Split(",");
+                        List<int> ids = new List<int>();
+                        foreach(var id in productIds)
+                        {
+                            ids.Add(Int32.Parse(id));
+                        }
+                        item.UserId = (int)reader["user_id"];
+
+                    }
+                }
+            }
+            return null;
+    }
     }
 }
--- src/Codecool.CodecoolShop/Models/CartItemModel.cs
+++ src/Codecool.CodecoolShop/Models/CartItemModel.cs
@@ -8,6 +8,8 @@ namespace Codecool.CodecoolShop.Models
     public class CartItemModel
     {
         public Product Product { get; set; }
+
+        public int UserId { get; set; }
         public int Quantity { get; set; }
         public CartItemModel()
         {
