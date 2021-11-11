47d6fe7044291de223a8b97fb1159968c02178d7
mihail-dev101 <73068128+mihail-dev101@users.noreply.github.com>
2021-11-11 12:24:38 +0200

    Added registration and signinmodel

--- src/Codecool.CodecoolShop/Controllers/ProductController.cs
+++ src/Codecool.CodecoolShop/Controllers/ProductController.cs
@@ -218,5 +218,98 @@ namespace Codecool.CodecoolShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Registration()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult SaveRegisterDetails(RegistrationModel registerDetails)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                CheckoutModel userDetails = new CheckoutModel();
+
+
+
+                ViewBag.Message = "User Details Saved";
+                return View("Register");
+            }
+            else
+            {
+
+                
+                return View("Register");
+            }
+        }
+
+
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Signin(SigninModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var isValidUser = IsValidUser(model);
+
+                //If user is valid & present in database, we are redirecting it to Welcome page.
+                if (isValidUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    //If the username and password combination is not present in DB then error message is shown.
+                    ModelState.AddModelError("Failure", "Wrong Username and password combination !");
+                    return View();
+                }
+            }
+            else
+            {
+                //If model state is not valid, the model with error message is returned to the View.
+                return View(model);
+            }
+        }
+
+        //function to check if User is valid or not
+        public RegistrationModel IsValidUser(SigninModel model)
+        {
+            using (var dataContext = new LoginRegistrationInMVCEntities())
+            {
+                //Retireving the user details from DB based on username and password enetered by user.
+                RegisterUser user = dataContext.RegisterUsers.Where(query => query.Email.Equals(model.Email) && query.Password.Equals(model.Password)).SingleOrDefault();
+                //If user is present, then true is returned.
+                if (user == null)
+                    return null;
+                //If user is not present false is returned.
+                else
+                    return user;
+            }
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("Index");
+        }
     }
+}
+}
 }
---
+++ src/Codecool.CodecoolShop/Models/SigninModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class SigninModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email Address!")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
